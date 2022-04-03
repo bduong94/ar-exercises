5688a65df99c10005cc4d05f9798e06cc4256f2e
Brian Duong <brianminhduong@gmail.com>
2022-04-03 19:40:15 +0000

    Count number of stores in database

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -12,3 +12,5 @@ end
 burnaby = Stores.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel:true);
 richmond = Stores.create(name: "Richmond", annual_revenue: 1260000 , mens_apparel: false, womens_apparel:true);
 gastown = Stores.create(name: "Gastown", annual_revenue: 190000 , mens_apparel: true, womens_apparel:false);
+
+puts Stores.count(:all)
